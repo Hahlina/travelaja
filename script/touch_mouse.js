36161fce2620493a6b18c6e8b90bb6cc63b68dad
Hahlina <hahlina@e-macis-MacBook-Pro.local>
2023-01-15 16:29:58 +0200

    done menu burger

--- script/touch_mouse.js
+++ script/touch_mouse.js
@@ -8,8 +8,21 @@ let isMobile = {
 };
 		let body=document.querySelector('body');
 if(isMobile.any()){
-		body.classList.add('touch');
+		body.classList.add('_touch');
 	
 }else{
-	body.classList.add('mouse');
+	body.classList.add('_pc');
 }
+
+
+var burgerMenu = document.getElementById('burger-menu');
+
+var overlay = document.querySelector('.nav');
+burgerMenu.addEventListener('click', function() {
+	document.body.classList.toggle('_lock');
+  this.classList.toggle("close");
+  overlay.classList.toggle("overlay");
+});
+
+
+
